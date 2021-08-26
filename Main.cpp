4bd4161a5dff056c63b31f4d5ae36f5805f99f39
niklas <ngutting@gmx.de>
2021-08-26 10:44:09 +0200

    Adding ability to read instructions from file

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include "Parser.h"
+
+#include <iostream>
+#include <fstream>
+
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {
+	std::cerr << "No inputfile given\n";
+	exit(1);
+    }
+    std::ifstream myfile {argv[1]};
+    if (!myfile.is_open()) {
+	std::cerr << "File " << argv[1] << " not found\n";
+	exit(1);
+    }
+    std::string line{};
+    while (getline(myfile, line)) {
+	Action action {parseLine(line)};
+	(*action.action)(action.arg);
+    }
+    
+    myfile.close();
+    return 0;
+}
