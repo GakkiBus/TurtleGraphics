97a7f8de8d8acdf0660615eecf530a4ae5737d30
niklas <ngutting@gmx.de>
2021-09-10 16:06:51 +0200

    main function starts Qt-Application

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "parser.h"
+#include "mainwindow.h"
+
+#include <QtWidgets>
 
 #include <iostream>
 #include <fstream>
@@ -6,21 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1) {
-	std::cerr << "No inputfile given\n";
-	exit(1);
-    }
-    std::ifstream myfile {argv[1]};
-    if (!myfile.is_open()) {
-	std::cerr << "File " << argv[1] << " not found\n";
-	exit(1);
-    }
-    std::string line{};
-    while (getline(myfile, line)) {
-	Action action {parseLine(line)};
-	(*action.action)(action.arg);
-    }
-    
-    myfile.close();
-    return 0;
+    QApplication app(argc, argv);
+
+    MainWindow window;
+    window.show();
+    return app.exec();
 }
